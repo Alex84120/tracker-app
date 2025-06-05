de205fa294dbb60fbb0249fb1d4462c1b7b7fd0f
Alex84120 <alexandrecastanier84@gmail.com>
2025-06-06 00:26:29 +0200

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-2">🏠 Accueil</h1>
+      <p>Bienvenue dans Tracker !</p>
+      <p>Tu peux naviguer via le menu ci-dessous pour suivre ton entraînement, ta nutrition, ton poids et plus encore.</p>
+    </div>
+  )
+}
