629468dbf6526a6044b5f9dfa5a29144dc6cdbc4
Alex84120 <alexandrecastanier84@gmail.com>
2025-06-06 11:33:30 +0200

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://your-project.supabase.co' // ← je te remplace l'URL ici
-const supabaseKey = 'your-anon-key' // ← ta clé Supabase publique ici
+const supabaseUrl = 'https://kvrjcopzrmtlklbmdrri.supabase.co' // ← je te remplace l'URL ici
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2cmpjb3B6cm10bGtsYm1kcnJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMDE2MjYsImV4cCI6MjA2NDc3NzYyNn0.cWME9-PH8smIv5t9w1Td2wbVIyaw5ivavahK4TpGrR0' // ← ta clé Supabase publique ici
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
